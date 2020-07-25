1b7ec4c0b6a0220851fa85f85284ffb8d1f05167
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2020-07-25 16:56:33 +0530

    main file with buildcontext to enable hot reload

---
+++ hot_app/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'UI/home.dart';
+
+main()
+{
+runApp(x());
+}
+
+class x extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return myapp();
+  }
+}
